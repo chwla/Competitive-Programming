10dad0647aa1ce7dae67891a01af32055892592f
chwla <chwla75@gmail.com>
2025-08-18 21:15:57 +0530

    1030A

---
+++ striver/Implementation/1030A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x;
+        cin >> x;
+        if (x == 1) {
+            cout << "HARD";
+            return 0;
+        }
+    }
+    cout << "EASY";
+}
